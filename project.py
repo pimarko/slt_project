4b5493253eeffc75e09faf54646203fb4705ee39
mgrapes <=pimarko@student.ethz.ch>
2016-05-18 14:39:45 +0200

    still buggy(probably) and definitely NOT optimised

--- project.py
+++ project.py
@@ -7,6 +7,9 @@ from sklearn.neighbors import BallTree
 from math import exp
 from random import randint
 from collections import Counter
+from mpl_toolkits.mplot3d import Axes3D
+import itertools
+import seaborn as sns
 
 #modify constants
 K_NN = 5
@@ -14,6 +17,8 @@ VOXELS_GRID = [5,5,5]
 Q = 10
 M = 100
 eta = 0.97
+GENERATE_PLOT_SEARCH_SPM = True
+GENERATE_PLOT_CLUSTERS = True
 
 #do not modify constants
 voxel_num = VOXELS_GRID[0]*VOXELS_GRID[1]*VOXELS_GRID[2]
@@ -145,6 +150,15 @@ def get_Ttrans():
  	coeff = float(4*np.log(1+np.sqrt(Q)))
  	return float(np.exp(-1/2))/float(coeff)
 
+
+def in_same_cluster_cij(cluster,i,j):
+	for key in clusters.keys():
+		values = clusters[key]
+		if((i in values) and (j in values)):
+			return True
+
+	return False
+
 #data import
 img = nib.load('diff_data.nii')
 bvecs = np.loadtxt('bvecs')
@@ -173,43 +187,106 @@ print "Data preprocessing - done."
 spins = np.random.randint(Q, size=(data_matrix.shape[0],1))
 data_matrix = np.concatenate((data_matrix,spins),axis = 1)
 #SW - MC 
-chi_temp = []
+if(GENERATE_PLOT_SEARCH_SPM):
+	chi_temp = []
 
-Ti = get_Ttrans() + 1 #superparamagnetic to paragmagnetic
-Tf = get_Ttrans() - float(get_Ttrans())/float(2) #ferromagnetic to superparamagnetic
-T = Ti
-temps = []
-iter_num = 0
-C_ij = np.zeros((voxel_num,voxel_num))
+	Ti = get_Ttrans() + 1 #superparamagnetic to paragmagnetic
+	Tf = get_Ttrans() - float(get_Ttrans())/float(2) #ferromagnetic to superparamagnetic
+	T = Ti
+	temps = []
+	iter_num = 0
+	C_ij = np.zeros((voxel_num,voxel_num))
+	cluster_num = []
+	while(T > Tf):
+		m_steps = []
+		for itr in range(M):
+			frozen_bounds_indices = frozen_bounds(J_matrix,T,data_matrix)
 
-while(T > Tf):
-	m_steps = []
-	for ii in range(M):
-		frozen_bounds_indices = frozen_bounds(J_matrix,T,data_matrix)
+			clusters = find_clusters(frozen_bounds_indices)
+			for key in clusters.keys():
+				values = clusters[key]
+				data_matrix[values,data_matrix.shape[1]-1] = np.random.randint(Q)
+			
+			N = count_spins(data_matrix[:,data_matrix.shape[1]-1])
+			m = get_m(N)
+			if(itr > 9):
+				m_steps.append(m)
 
-		clusters = find_clusters(frozen_bounds_indices)
-		for key in clusters.keys():
-			values = clusters[key]
-			data_matrix[values,data_matrix.shape[1]-1] = np.random.randint(Q)
 		
-		N = count_spins(data_matrix[:,data_matrix.shape[1]-1])
-		m = get_m(N)
-		if(ii > 9):
-			m_steps.append(m)
+		temps.append(T)
+		chi = get_chi(m_steps,T)
+		chi_temp.append(chi)
+		cluster_num.append(len(clusters.keys()))
 
-	chi = get_chi(m_steps,T)
-	chi_temp.append(chi)
-	temps.append(T)
-	T = Ti*(np.power(eta,iter_num))
-	iter_num = iter_num + 1
-	print iter_num
+		T = Ti*(np.power(eta,iter_num))
+		iter_num = iter_num + 1
+		print iter_num
+
+	plt.plot(temps,cluster_num,'o')
+	plt.xlabel('temperature')
+	plt.ylabel('cluster number')
+	plt.title('Relation between cluster number and temperature')
+	plt.show()
+
+	plt.plot(temps,chi_temp,'o')
+	plt.xlabel('temperature')
+	plt.ylabel('chi')
+	plt.title('Searching for super paramagnetic range')
+	plt.show()
+
+if(GENERATE_PLOT_CLUSTERS):
+	#save C_ij in the MC, measure spin spin, get the threshold and build the clusters
 	
-plt.plot(temps,chi_temp,'o')
-plt.xlabel('temperature')
-plt.ylabel('chi')
-plt.title('Searching for super paramagnetic range')
+	T_spm = 0.2 #set it to that or smth in the range (finer the range on the plot, the better we see the constant in the superparamagnetic phase)
+
+	for itr in range(M):
+			frozen_bounds_indices = frozen_bounds(J_matrix,T_spm,data_matrix)
+
+			clusters = find_clusters(frozen_bounds_indices)
+			for key in clusters.keys():
+				values = clusters[key]
+				data_matrix[values,data_matrix.shape[1]-1] = np.random.randint(Q)
+
+			if(itr > 9):
+				for ii in range(voxel_coord_matrix.shape[0]):
+					for jj in range(ii+1,voxel_coord_matrix.shape[0]):
+						if(in_same_cluster_cij(clusters,ii,jj)):
+							C_ij[ii,jj] = C_ij[ii,jj] + 1
+					
+
+
+			
+			print itr
+
+	C_ij = C_ij/M # upper triangular
+	C_ij = C_ij*(Q-1)
+	G_ij = C_ij + np.ones((C_ij.shape[0],C_ij.shape[1]))
+	G_ij = G_ij/Q # not upper triangluar- just observe the upper triangular part
+
+	bound_clusters = dict()
+	for ii in range(G_ij.shape[0]):
+		bound_clusters[ii] = []
+		for jj in range(ii+1,G_ij.shape[1]):
+			if(G_ij[ii,jj] > 0.5):
+				bound_clusters[ii].append(jj)
+
+	new_clusters = find_clusters(bound_clusters)
+
+	fig = plt.figure()
+	ax = fig.add_subplot(111, projection='3d')
+	palette = itertools.cycle(sns.color_palette())
+	for key in new_clusters.keys():
+		values = new_clusters[key] #all voxels in this cluster; voxel numbers
+		iss = voxel_coord_matrix[values,0]
+		jss = voxel_coord_matrix[values,1]
+		kss = voxel_coord_matrix[values,2]
+
+	
+		c=next(palette)
+		ax.scatter(iss, jss, kss,c = c, marker = ',',s = 1000)
+
+ax.set_xlabel('i Label')
+ax.set_ylabel('j Label')
+ax.set_zlabel('k Label')
+
 plt.show()
-
-#save C_ij in the MC, measure spin spin, get the threshold and build the clusters
-
-
