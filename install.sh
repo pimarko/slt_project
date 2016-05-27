86c3d8c0184c2465719f66ef6e8431f7306cfa46
Nico Previtali <nicoprevitali@users.noreply.github.com>
2016-05-27 17:56:02 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,5 @@
-
-
 pip install sklearn
 pip install numpy
 pip install nibabel
 pip install matplotlib
 pip install scipy
-
-pip install math
-
-
-# import sys
-
-# from random import randint
-# from collections import Counter
-# from mpl_toolkits.mplot3d import Axes3D
-# import itertools
-# import find_clusters
-
-
